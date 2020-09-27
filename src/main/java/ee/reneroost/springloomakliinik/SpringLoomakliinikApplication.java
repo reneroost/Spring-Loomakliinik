1a968d25fef0e7c1b5319dbef3237df5fe470159
Rene.Roost <rene.roost@ttu.ee>
2020-09-27 17:45:01 +0300

    Genereeritud projekti lisamine

---
+++ src/main/java/ee/reneroost/springloomakliinik/SpringLoomakliinikApplication.java
@@ -0,0 +1,13 @@
+package ee.reneroost.springloomakliinik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLoomakliinikApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLoomakliinikApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ee/reneroost/springloomakliinik/SpringLoomakliinikApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.reneroost.springloomakliinik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLoomakliinikApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
